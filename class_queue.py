c632b0cfba4962be8f8b671a2c2b310cf5ed89e0
mario-marin <mario.marin.13@sansano.usm.cl>
2018-06-15 18:30:53 -0400

    class_queue and class_server tweaked

--- class_queue.py
+++ class_queue.py
@@ -7,6 +7,14 @@ Created on 04-06-2018
 import numpy
 import random
 
+
+class queue:
+    def __init__(self,capacidad_de_colas):
+        self.capacidad = capacidad_de_colas
+        self.usage = 0
+        self.list = []
+
+
 class queue_layer(object):
     '''
     classdocs
@@ -22,7 +30,7 @@ class queue_layer(object):
         self.tasa_de_abandono = tasa_de_abandono
         self.queue_list = [] #idea similar eal server_list
         for x in range(0,numero_de_colas):
-            self.queue_list.append([capacidad_de_colas,0,[]]) #el 0 es el uso inicial de la cola, por otro lado el arreglo final corresponde a los tiempos de abandono de cada usuario en la cola
+            self.queue_list.append(queue(capacidad_de_colas)) #el 0 es el uso inicial de la cola, por otro lado el arreglo final corresponde a los tiempos de abandono de cada usuario en la cola
     
     def select_queue(self): #metodo decide a que cola enviar al usuario entrante (enviedo desde el server), si todas las colas estan en la pasta entonses botar la coneccion (bloqueo)
         return random.choice(range(0,self.numero_de_colas))
@@ -34,13 +42,13 @@ class queue_layer(object):
             return random.choice(queue_state)
         
     def queue_full_query(self,queue_id):
-        if self.queue_list[queue_id][0] <= self.queue_list[queue_id][1]: #si la capacidad es mayor al uso
+        if self.queue_list[queue_id].capacidad <= self.queue_list[queue_id].usage: #si la capacidad es menur al uso
             return True
         else:
             return False
         
     def queue_empty(self,queue_id):
-        if len(self.queue_list[queue_id][2]) == 0:
+        if len(self.queue_list[queue_id].list) == 0:
             return True
         else:
             return False
@@ -50,34 +58,34 @@ class queue_layer(object):
         not_empty_queue = list(range(0,self.numero_de_colas))
         
         for queue_id in range(0,self.numero_de_colas):
-            if self.queue_list[queue_id][1] == 0:
+            if self.queue_list[queue_id].usage == 0:
                 not_empty_queue.remove(queue_id)
         return not_empty_queue    
     
         
     def add_to_queue(self,queue_id,sim_time):
         desertion_time = numpy.random.exponential(self.tasa_de_abandono) + sim_time
-        self.queue_list[queue_id][1] = self.queue_list[queue_id][1] + 1
-        self.queue_list[queue_id][2].append(desertion_time)
-        self.queue_list[queue_id][2].sort()
-        print("added to queue")
+        self.queue_list[queue_id].usage = self.queue_list[queue_id].usage + 1
+        self.queue_list[queue_id].list.append(desertion_time)
+        self.queue_list[queue_id].list.sort()
+        print("fucker added to queue")
         
     def pop_queue(self,queue_id): #metodo ingresa a el usuario siguiente a el sistema (este usuario debe ser atendido y no puede ser rechazado por el server)
-        self.queue_list[queue_id][1] = self.queue_list[queue_id][1] - 1
-        print(self.queue_list[queue_id][2].pop(0))
+        self.queue_list[queue_id].usage = self.queue_list[queue_id].usage - 1
+        print(self.queue_list[queue_id].list.pop(0))
         
     def update_queue(self,sim_time):
         
-        for x in range(0,len(self.queue_list)):
+        for x in range(0,self.numero_de_colas):
             index = 0
-            for i in range(0,len(self.queue_list[x][2])):
-                if self.queue_list[x][2][i] < sim_time:
+            for i in range(0,self.queue_list[x].usage):
+                if self.queue_list[x].list[i] < sim_time:
                     index = index + 1
                 else:
                     break
             while index != 0:
                 index  = index - 1
-                self.queue_list[x][1] = self.queue_list[x][1] - 1
-                self.queue_list[x][2].pop(0)
+                self.queue_list[x].usage = self.queue_list[x].usage - 1
+                self.queue_list[x].list.pop(0)
     
         
--- class_server.py
+++ class_server.py
@@ -2,7 +2,13 @@
 Created on 04-06-2018
 
 @author: mario
+
 '''
+
+class server:
+    def __init__(self,capacity):
+        self.usage = 0
+        self.capacity = capacity
 
 class server_layer(object):
     '''
@@ -18,12 +24,12 @@ class server_layer(object):
         self.capacity = capacity
         self.server_list = [] #arreglo donde cada indice reŕecenta un servidor, y cada indice contiene informacion de ese servidor (por ahora solo su tasa de atencion, numero de bloqueos y capacidad total)
         for x in range(0,number_of_servers):
-            self.server_list.append([0,capacity])
+            self.server_list.append(server(capacity))
        
        
        
     def server_full_query(self,server_id):
-        if self.server_list[server_id][1] <= self.server_list[server_id][0]:
+        if self.server_list[server_id].capacity <= self.server_list[server_id].usage:
             return True
         else:
             return False  
@@ -33,16 +39,16 @@ class server_layer(object):
         available_servers = list(range(0,self.number_of_servers))
         
         for server_id in range(0,self.number_of_servers):
-            if self.server_list[server_id][1] <= self.server_list[server_id][0]:
+            if self.server_list[server_id].capacity <= self.server_list[server_id].usage:
                 available_servers.remove(server_id)
         return available_servers
         
         
     def increase_usage(self,server_id):
-        self.server_list[server_id][0] = self.server_list[server_id][0] + 1
+        self.server_list[server_id].usage = self.server_list[server_id].usage + 1
             
     def decrease_usage(self,server_id):
-        self.server_list[server_id][0] = self.server_list[server_id][0] - 1
+        self.server_list[server_id].usage = self.server_list[server_id].usage - 1
     '''
             
     def process_load(self): #metodo porsesa al usuario entrante, viendo si es en efecto atendido (ocupando al servidor) o es enviado a una cola    
